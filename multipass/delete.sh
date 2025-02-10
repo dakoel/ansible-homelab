47885ff342b557a6df8132dfbba6af7058c71463
Daniel Ellert <dkedevelop@pm.me>
2025-02-10 13:47:11 +0100

    Improve Taskfile

--- multipass/delete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-multipass stop control
-multipass stop worker1
-multipass stop worker2
-multipass delete control
-multipass delete worker1
-multipass delete worker2
-multipass purge
-nmcli connection delete multipassbr
